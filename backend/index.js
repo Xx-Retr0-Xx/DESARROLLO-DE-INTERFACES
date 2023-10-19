312a3a72338d06a01458e073f6a450102099d171
MarcosMayor <muijyt@gmai.com>
2023-10-19 10:23:08 +0100

    :wq

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+app = express()
+const port = 3030
+
+app.get('/', function (req, res) {
+    res.json({message: 'Hola Mundo!'})
+})
+
+app.listen(port)   
+console.log('API escuchando desde el puerto ' + port)
