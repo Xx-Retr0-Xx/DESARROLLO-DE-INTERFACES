13dd03b6612a7dc7c80e6e94f445aa6ce0b64678
MarcosMayor <muijyt@gmai.com>
2023-10-23 09:10:16 +0100

    clase 2 23/10

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import Button from '@mui/material/Button'
-import {Container} from '@mui/material';
-import {Typography} from '@mui/material';
+import {Box, TextField} from '@mui/material';
+import { useState } from 'react';
 
 function Login () {
+
+const [login, setLogin] = useState('')
+
+const isVerifiedUser = () => {
+    fetch(`http://localhost:3030/login?user=${login}&password=${'123456789'}`)
+    .then(response => response.json())
+    .then(response => {
+        if (response) {
+          console.log(response)   
+        }
+    })
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    isVerifiedUser()
+    console.log(login)
+}
+
 return <>
-<Container>
-    <Typography variant="h1" component="h2">App con typography</Typography>
-    <Button color="success" variant="contained"> Hola </Button>
-</Container>
+    <Box component='form' onSubmit={handleSubmit}>
+        <TextField 
+            id='login' 
+            label='Usuario' 
+            variant='outlined' 
+            fullWidth
+            autoFocus
+
+            value={login}
+            onChange={(event) => setLogin(event.target.value)}
+        />
+
+        <Button 
+            type="submit" 
+            variant='contained' 
+            fullWidth
+        >
+         Acceder
+        </Button>
+    </Box>
 </>
 }
 export default Login
+
+/*
+<Container>
+    <Typography variant="h1" component="h2">App con typography</Typography>
+    <Button color="success" variant="contained"> Hola </Button>
+</Container>*/
